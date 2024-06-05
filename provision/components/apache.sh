fb0b9992f41d089b2e6ecf2051929f9a11035958
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2024-06-05 11:18:32 -0400

    local vm with vagrant, ssl certificate and force https

---
+++ provision/components/apache.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y apache2
+
+# Copy the vhost config file
+cp /var/www/provision/config/apache/vhosts/weepingplebs.local.conf /etc/apache2/sites-available/weepingplebs.local.conf
+
+# Disable the default vhost file
+a2dissite 000-default
+
+# Enable our custom vhost files
+a2ensite weepingplebs.local.conf
+
+# Enable ModRewrite
+a2enmod rewrite
+
+# Restart for the changes to take effect
+service apache2 restart
+
+# ssl certificate for domain
+sudo apt install openssl
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/ssl/private/weepingplebs.local.key \
+    -out /etc/ssl/certs/weepingplebs.local.crt \
+    -subj "/C=US/ST=CA/L=San Francisco/O=Example, Inc./OU=IT Department/CN=weepingplebs.local"
+
+sudo a2enmod ssl
+sudo systemctl restart apache2
